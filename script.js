4a9e7d7c26719be45f0fb86f7dee012c09515f5d
aarpatil09-lgtm <aarpatil09@gmail.com>
2026-04-28 12:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let result = "";
+	for(let i=0;i < l;i++){
+		let index = Math.floor(Math.random * char.length);
+		result += chars[index];
+	}
+	return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+ const l = prompt("Enter a number.");
+ alert(makeid(l));
